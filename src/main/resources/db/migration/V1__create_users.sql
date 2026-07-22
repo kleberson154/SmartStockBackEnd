473b35eea03432de9a03f8dd6f23d0e52339f2b2
kleberson154 <kleberson55@hotmail.com>
2026-07-22 17:29:50 -0300

    chore: configure PostgreSQL and Flyway

---
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users
+(
+    id UUID PRIMARY KEY,
+    name VARCHAR(120) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(30) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
