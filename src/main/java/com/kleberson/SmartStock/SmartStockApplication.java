eba57042be3bc2a316ec00865f03a1d5b35a8a98
kleberson154 <kleberson55@hotmail.com>
2026-07-22 16:22:26 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/kleberson/SmartStock/SmartStockApplication.java
@@ -0,0 +1,13 @@
+package com.kleberson.SmartStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kleberson/SmartStock/SmartStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleberson.SmartStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
