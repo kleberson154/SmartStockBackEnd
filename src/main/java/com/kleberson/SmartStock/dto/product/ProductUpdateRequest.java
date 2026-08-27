02c2a1c637a140c452efa113dd11521f49c039ef
kleberson154 <kleberson55@hotmail.com>
2026-08-27 15:16:15 -0300

    feat: create ProductUpdateRequest DTO for updating product information

---
+++ src/main/java/com/kleberson/SmartStock/dto/product/ProductUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.kleberson.SmartStock.dto.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductUpdateRequest {
+    private String name;
+    private String code;
+    private String category;
+    private BigDecimal price;
+    private Integer quantity;
+    private Integer minimumStock;
+}
