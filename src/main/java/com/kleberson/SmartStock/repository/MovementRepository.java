8034ed51e2eea0e125e3e7ab654771b1f7369597
kleberson154 <kleberson55@hotmail.com>
2026-08-27 15:15:22 -0300

    feat: add method to find movements by product ID in MovementRepository

--- src/main/java/com/kleberson/SmartStock/repository/MovementRepository.java
+++ src/main/java/com/kleberson/SmartStock/repository/MovementRepository.java
@@ -3,7 +3,9 @@ package com.kleberson.SmartStock.repository;
 import com.kleberson.SmartStock.entity.Movement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MovementRepository extends JpaRepository<Movement, UUID> {
+    List<Movement> findByProductId(UUID productId);
 }
