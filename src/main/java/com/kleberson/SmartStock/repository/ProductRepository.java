43e9a0f88968402ea595f752f0796300e2009d7b
kleberson154 <kleberson55@hotmail.com>
2026-08-27 15:15:15 -0300

    feat: add query to find low stock products in ProductRepository

--- src/main/java/com/kleberson/SmartStock/repository/ProductRepository.java
+++ src/main/java/com/kleberson/SmartStock/repository/ProductRepository.java
@@ -2,10 +2,18 @@ package com.kleberson.SmartStock.repository;
 
 import com.kleberson.SmartStock.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     Optional<Product> findByCode(String code);
+    @Query("""
+        SELECT p
+        FROM Product p
+        WHERE p.quantity <= p.minimumStock
+    """)
+    List<Product> findLowStockProducts();
 }
