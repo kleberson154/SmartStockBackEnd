b88d51f14e088a9698a26cc3585f6c6b3cb7226a
kleberson154 <kleberson55@hotmail.com>
2026-08-28 19:44:41 -0300

    feat: enhance OpenAPI configuration with JWT security scheme

--- src/main/java/com/kleberson/SmartStock/config/OpenApiConfig.java
+++ src/main/java/com/kleberson/SmartStock/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.kleberson.SmartStock.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,16 +15,36 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI smartStockOpenAPI() {
+
+        String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
-                    .title("Smart Stock API")
-                    .description("API REST para gerenciamento de estoque")
-                    .version("1.0.0")
-                            .contact(new Contact()
-                                    .name("Kleberson")
-                                    .email("kleberson55@hotmail.com")))
+                        .title("Smart Stock API")
+                        .description("API REST para gerenciamento de estoque")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Kleberson")
+                                .email("kleberson55@hotmail.com")))
                 .externalDocs(new ExternalDocumentation()
-                    .description("Repositório do projeto")
-                    .url("https://github.com/kleberson154/SmartStockBackEnd"));
+                        .description("Repositório do projeto")
+                        .url("https://github.com/kleberson154/SmartStockBackEnd"))
+
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemeName)
+                )
+
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
     }
 }
