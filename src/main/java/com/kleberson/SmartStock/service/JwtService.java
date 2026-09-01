cd96b83367ffd5d8a67398f2e9b3d5106f2aad0c
kleberson154 <kleberson55@hotmail.com>
2026-09-01 02:50:58 -0300

    feat: update JwtService to use User entity for token generation and include user role

--- src/main/java/com/kleberson/SmartStock/service/JwtService.java
+++ src/main/java/com/kleberson/SmartStock/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.kleberson.SmartStock.service;
 
+import com.kleberson.SmartStock.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
@@ -12,26 +13,32 @@ import java.util.Date;
 
 @Service
 public class JwtService {
+
     @Value("${app.security.jwt.secret}")
     private String secret;
 
     @Value("${app.security.jwt.expiration}")
     private Long expiration;
 
-    private SecretKey getSigningKey(){
-        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
     }
 
-    public String generateToken(String email){
+    public String generateToken(User user) {
         return Jwts.builder()
-                .subject(email)
+                .subject(user.getEmail())
+                .claim("role", user.getRole().name())
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .expiration(
+                        new Date(System.currentTimeMillis() + expiration)
+                )
                 .signWith(getSigningKey())
                 .compact();
     }
 
-    public String extractToken(String token){
+    public String extractToken(String token) {
         Claims claims = Jwts.parser()
                 .verifyWith(getSigningKey())
                 .build()
@@ -41,12 +48,13 @@ public class JwtService {
         return claims.getSubject();
     }
 
-    public boolean isTokenValid(String token){
+    public boolean isTokenValid(String token) {
         try {
             Jwts.parser()
                     .verifyWith(getSigningKey())
                     .build()
                     .parseSignedClaims(token);
+
             return true;
         } catch (Exception ex) {
             return false;
